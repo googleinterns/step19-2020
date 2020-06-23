022e5e025b75ea7ac942c5b84481ce81638c2ac7
niuamy <aniu@google.com>
2020-06-23 16:16:32 -0400

    Add carousel of trending topic titles

--- project/src/main/webapp/script.js
+++ project/src/main/webapp/script.js
@@ -1,28 +1,29 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+function switchTrend(val) {
+  var trends = document.getElementsByClassName('trends');
+  var i = 0;
+  var updatedCurrentSlide = false;
+  while(i < trends.length && !updatedCurrentSlide) {
+    if(trends[i].style.display === 'block') {
+      var nextTrend = getNextTrend(trends[i],val);
+      trends[i].style.display = 'none';
+      nextTrend.style.display = 'block';
+      updatedCurrentSlide = true;
+    }
+    i+=1;
+  }
+}
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+function getNextTrend(trend, val) {
+  var nextSlide = -1;
+  var currentTrendVal = parseInt(trend.getAttribute('value'));
+  // Clicking left on the first slide returns to last slide
+  if (currentTrendVal + val === 0) {
+    nextSlide = 4;
+  // Clicking right on the last slide returns to first slide  
+  } else if (currentTrendVal + val === 5) {
+    nextSlide = 1;
+  } else {
+    nextSlide = currentTrendVal + val;
+  }
+  return document.getElementById('trend-' + nextSlide);
 }
