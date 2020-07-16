4c6f71b15e5cf07d22f86ae2c02f04c6d5488680
acnwigwe <acnwigwe@google.com>
2020-07-15 21:07:04 -0400

    Add functionality for locating user's country

---
+++ project/src/main/java/com/google/sps/servlets/GeoServlet.java
@@ -0,0 +1,20 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import com.google.gson.Gson;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.sps.data.GeoService;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/geo")
+public class GeoServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      GeoService geoService = new GeoService();
+      geoService.getValidCountries();
+
+    }
+}
