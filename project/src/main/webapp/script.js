8fd947d9a45fed2c9c8862c202868e6a7b314ba4
Amy <amyyniu@gmail.com>
2020-07-17 14:40:54 +0000

    Translate main page

--- project/src/main/webapp/script.js
+++ project/src/main/webapp/script.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-unused-vars */
-const basePath = '/news';
-const numParam = 'num=';
+const basePath = "/news";
+const numParam = "num=";
 const trend = {
-  content: '',
+  content: "",
   currentTrendVal: 1,
-  color: '',
+  color: "",
 };
 
 /** Show and hide nav bar. */
 function toggleNavBar() {
-  const nav = document.getElementById('nav');
-  const main = document.getElementById('main');
-  if (nav.style.display === 'none' || nav.style.display === '') {
-    nav.style.display = 'block';
+  const nav = document.getElementById("nav");
+  const main = document.getElementById("main");
+  if (nav.style.display === "none" || nav.style.display === "") {
+    nav.style.display = "block";
     if (screen.width < 1024) {
-      main.style.display = 'none';
+      main.style.display = "none";
     }
   } else {
-    nav.style.display = 'none';
+    nav.style.display = "none";
     if (screen.width < 1024) {
-      main.style.display = 'block';
+      main.style.display = "block";
     }
   }
 }
@@ -41,13 +41,13 @@ function switchTrend(val, arrow, trends) {
   } else {
     nextTrend = val;
   }
-  const oldDot = document.getElementById('dot-' + trend.currentTrendVal);
-  const newDot = document.getElementById('dot-' + nextTrend);
-  oldDot.style.backgroundColor = 'transparent';
-  newDot.style.backgroundColor = 'rgb(226, 226, 226)';
+  const oldDot = document.getElementById("dot-" + trend.currentTrendVal);
+  const newDot = document.getElementById("dot-" + nextTrend);
+  oldDot.style.backgroundColor = "transparent";
+  newDot.style.backgroundColor = "rgb(226, 226, 226)";
   trend.currentTrendVal = nextTrend;
   if (trend.content.length === 0) {
-    return '';
+    return "";
   } else {
     getTrends(trend.currentTrendVal, trend.content);
     getArticles(trend.currentTrendVal, trend.content);
@@ -65,7 +65,7 @@ function getNextTrendValue(direction, trendVal) {
   // Clicking left on the first slide returns to last slide
   if (nextSlide === 0) {
     nextSlide = 4;
-  // Clicking right on the last slide returns to first slide
+    // Clicking right on the last slide returns to first slide
   } else if (nextSlide === 5) {
     nextSlide = 1;
   }
@@ -75,9 +75,9 @@ function getNextTrendValue(direction, trendVal) {
 
 /** Shows main page after page load. */
 function showPage() {
-  document.getElementById('preloader').style.display = 'none';
-  const content = document.getElementById('content');
-  content.style.cssText = 'display: block; animation: fade-in 2.5s linear';
+  document.getElementById("preloader").style.display = "none";
+  const content = document.getElementById("content");
+  content.style.cssText = "display: block; animation: fade-in 2.5s linear";
 }
 
 /**
@@ -87,7 +87,7 @@ function showPage() {
  * @param {object} trend Holds the trends and articles retrieved.
  */
 async function retrieveArticles(numArticles, trend) {
-  const requestURL = basePath + '?' + numParam + numArticles;
+  const requestURL = basePath + "?" + numParam + numArticles;
   const response = await fetch(requestURL);
   trend.content = await response.json();
   getArticles(trend.currentTrendVal, trend.content);
@@ -101,15 +101,15 @@ async function retrieveArticles(numArticles, trend) {
  * @param {Array} content Array of Topic objects.
  */
 function getArticles(val, content) {
-  const trend = content[val-1];
+  const trend = content[val - 1];
   const articles = trend.articles;
-  const articleContainer = document.getElementById('article-container');
-  articleContainer.innerHTML = '';
+  const articleContainer = document.getElementById("article-container");
+  articleContainer.innerHTML = "";
   let right = false;
   articles.forEach((article) => {
     const child = createArticleElement(article, right);
     articleContainer.appendChild(child);
-    right = (!right) ? true : false;
+    right = !right ? true : false;
   });
 }
 
@@ -120,25 +120,25 @@ function getArticles(val, content) {
  * @return {HTMLDivElement} The article element.
  */
 function createArticleElement(article, right) {
-  const articleElement = document.createElement('div');
-  articleElement.className = (!right) ? 'articles' : 'articles right-justified';
+  const articleElement = document.createElement("div");
+  articleElement.className = !right ? "articles" : "articles right-justified";
 
-  const linkElement = document.createElement('a');
-  linkElement.setAttribute('href', article.link);
+  const linkElement = document.createElement("a");
+  linkElement.setAttribute("href", article.link);
 
-  const titleElement = document.createElement('h1');
-  titleElement.className = 'headers';
+  const titleElement = document.createElement("h1");
+  titleElement.className = "headers";
   // Removes source from title
   const newEnd = article.title.length - article.source.length - 3;
   const articleTitle = article.title.substring(0, newEnd);
   titleElement.innerText = articleTitle;
-  const authorElement = document.createElement('h3');
-  authorElement.className = 'subHeaders';
-  authorElement.innerText = 'author: ' + article.source;
+  const authorElement = document.createElement("h3");
+  authorElement.className = "subHeaders";
+  authorElement.innerText = "author: " + article.source;
 
-  const dateElement = document.createElement('h3');
-  dateElement.className = 'subHeaders';
-  dateElement.innerText = 'date: ' + article.pubDate;
+  const dateElement = document.createElement("h3");
+  dateElement.className = "subHeaders";
+  dateElement.innerText = "date: " + article.pubDate;
   linkElement.appendChild(titleElement);
   articleElement.appendChild(linkElement);
   articleElement.appendChild(authorElement);
@@ -153,9 +153,9 @@ function createArticleElement(article, right) {
  * @param {Array} content Array of Topic objects.
  */
 function getTrends(val, content) {
-  const trendContainer = document.getElementById('trend-container');
-  trendContainer.innerHTML = '';
-  const trend = content[val-1];
+  const trendContainer = document.getElementById("trend-container");
+  trendContainer.innerHTML = "";
+  const trend = content[val - 1];
   trendContainer.appendChild(createTrendElement(trend.name, val));
 }
 
@@ -166,9 +166,9 @@ function getTrends(val, content) {
  * @return {HTMLDivElement} The trend element.
  * */
 function createTrendElement(trend, val) {
-  const trendElement = document.createElement('div');
-  trendElement.className = 'trends';
-  trendElement.setAttribute('id', 'trend-' + val);
+  const trendElement = document.createElement("div");
+  trendElement.className = "trends";
+  trendElement.setAttribute("id", "trend-" + val);
   trendElement.innerText = trend;
   return trendElement;
 }
@@ -179,11 +179,11 @@ function createTrendElement(trend, val) {
  * @param {Map} colors Map of sentiments and their color assignments.
  */
 function getTrendBubbles(content, colors) {
-  const bubbleFirstRow = document.getElementById('frequency-row-1');
-  const bubbleSecondRow = document.getElementById('frequency-row-2');
+  const bubbleFirstRow = document.getElementById("frequency-row-1");
+  const bubbleSecondRow = document.getElementById("frequency-row-2");
   const bubbleSizes = getTrendBubbleSize(content);
   const sentimentScore = getTrendBubbleScore(content);
-  const length = (bubbleSizes.size)/2;
+  const length = bubbleSizes.size / 2;
   let i = 0;
   for (const [key, value] of bubbleSizes.entries()) {
     const color = colors.get(sentimentScore.get(key));
@@ -205,14 +205,14 @@ function getTrendBubbles(content, colors) {
  * @return {HTMLDivElement} The trend bubble element.
  */
 function createTrendBubble(trend, size, color) {
-  const bubbleElement = document.createElement('div');
-  bubbleElement.className = 'bubbles';
-  let style = '';
-  style = addStyleProperty(style, 'width', size + 'vw');
-  style = addStyleProperty(style, 'height', size + 'vw');
-  style = addStyleProperty(style, 'font-size', size/10 +'vw');
-  style = addStyleProperty(style, 'background-color', color);
-  bubbleElement.setAttribute('style', style);
+  const bubbleElement = document.createElement("div");
+  bubbleElement.className = "bubbles";
+  let style = "";
+  style = addStyleProperty(style, "width", size + "vw");
+  style = addStyleProperty(style, "height", size + "vw");
+  style = addStyleProperty(style, "font-size", size / 10 + "vw");
+  style = addStyleProperty(style, "background-color", color);
+  bubbleElement.setAttribute("style", style);
   bubbleElement.innerText = trend;
 
   return bubbleElement;
@@ -226,7 +226,7 @@ function createTrendBubble(trend, size, color) {
  * @return {string} The style string with added properties.
  */
 function addStyleProperty(style, property, value) {
-  return style.concat(property, ':', value, ';');
+  return style.concat(property, ":", value, ";");
 }
 
 /**
@@ -239,7 +239,7 @@ function getTrendBubbleSize(content) {
   const min = content[0].frequency;
   const proportions = [];
   for (let i = 0; i < content.length; i++) {
-    proportions[i] = content[i].frequency/max;
+    proportions[i] = content[i].frequency / max;
   }
   const size = new Map();
   content.forEach((trend) => {
@@ -282,7 +282,7 @@ function getTrendBubbleScore(trends) {
  * Returns the average of the sentiment scores of all the articles.
  * @param {Array} articles Array of all the articles for a trend.
  * @return {number} The average of the sentiment of all of the articles.
-*/
+ */
 function getAverageSentiment(articles) {
   const score = [];
   articles.forEach((article) => score.push(article.sentiment));
@@ -292,19 +292,71 @@ function getAverageSentiment(articles) {
 
 /**
  * Assigns and returns a color based on the sentiment value.
- * @return {string} the color of the sentiment.
-*/
+ * @return {string} The color of the sentiment.
+ */
 function getColorGradient() {
   const sentimentValueCount = 20;
-  const chromaColor = chroma.scale(['#FED8F7', '#C4DDFE']);
+  const chromaColor = chroma.scale(["#FED8F7", "#C4DDFE"]);
   const gradient = chromaColor.colors(sentimentValueCount);
   const colors = new Map();
   let j = 1;
   for (let i = 0; i <= gradient.length; i++) {
     colors.set(j.toFixed(1), gradient[i]);
-    j -= .1;
+    j -= 0.1;
   }
   return colors;
+}
+
+/**
+ * Updates the language on the UI.
+ * @param {string} language The language to change to.
+ */
+function changeLanguage(language) {
+  const translation = getTranslation(language);
+  const title = document.getElementById("title");
+  title.innerText = translation.title;
+  title.setAttribute("style", translation.style);
+  document.getElementById("home").innerText = translation.home;
+  document.getElementById("trends").innerText = translation.trends;
+  document.getElementById("topics").innerText = translation.topics;
+}
+
+/**
+ * Returns an object with the translated messages and new styles.
+ * @param {string} language The language the messages are to be translated into.
+ * @return {object} The translated messages and corresponding styles.
+ */
+function getTranslation(language) {
+  let title = "trending topics";
+  let home = "home";
+  let trends = "trends";
+  let topics = "topics";
+  let style = "";
+
+  if (language === "cn") {
+    title = "热门话题";
+    home = "主页";
+    trends = "趋势";
+    topics = "主题";
+    style = addStyleProperty(style, "font-size", "7vw");
+    style = addStyleProperty(style, "padding-left", "13vw");
+  } else if (language === "es") {
+    title = "tendencia de los temas";
+    home = "inicio";
+    trends = "tendencias";
+    topics = "temas";
+    style = addStyleProperty(style, "font-size", "5vw");
+    style = addStyleProperty(style, "transforms", "translate(-23%, -70%)");
+  }
+  const translation = {
+    language: language,
+    title: title,
+    home: home,
+    trends: trends,
+    topics: topics,
+    style: style,
+  };
+  return translation;
 }
 
 /** Shows preloader on page load and fetches articles. */
