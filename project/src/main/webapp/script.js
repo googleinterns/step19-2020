88d0bd23a961b8089c05b182c06c860570cb7754
Amy <amyyniu@gmail.com>
2020-07-23 21:17:11 +0000

    Update preloader design

--- project/src/main/webapp/script.js
+++ project/src/main/webapp/script.js
@@ -319,7 +319,8 @@ function changeLanguage(language) {
   document.getElementById("home").innerText = translation.home;
   document.getElementById("trends").innerText = translation.trends;
   document.getElementById("topics").innerText = translation.topics;
-  document.getElementById("sentiment-key-title").innerText = translation.sentiment;
+  document.getElementById("sentiment-key-title").innerText =
+    translation.sentiment;
   document.getElementById("neg").innerText = translation.negative;
   document.getElementById("pos").innerText = translation.positive;
   const sentiment = document.getElementById("sentiment-container");
@@ -361,7 +362,11 @@ function getTranslation(language) {
     sentiment = "puntuación de sentimiento";
     textSize = "";
     titleStyle = addStyleProperty(titleStyle, "font-size", "5vw");
-    titleStyle = addStyleProperty(titleStyle, "transforms", "translate(-23%, -70%)");
+    titleStyle = addStyleProperty(
+      titleStyle,
+      "transforms",
+      "translate(-23%, -70%)"
+    );
   }
   const translation = {
     language: language,
@@ -373,14 +378,56 @@ function getTranslation(language) {
     sentiment: sentiment,
     negative: negative,
     positive: positive,
-    textSize: textSize
+    textSize: textSize,
   };
   return translation;
 }
 
+/** Displays preloader. */
+function animatePreloader() {
+  let tl = anime.timeline({
+    easing: "easeOutExpo",
+    duration: 2600,
+  });
+
+  tl.add({
+    targets: ".square",
+    translateX: 200,
+    borderRadius: ["0%", "50%"],
+    backgroundColor: "#000000",
+    easing: "easeInOutQuad",
+  });
+
+  tl.add({
+    targets: ".square",
+    translateY: 200,
+    borderRadius: ["50%", "0"],
+    backgroundColor: "#4c4c4c",
+    easing: "easeInOutQuad",
+  });
+
+  tl.add({
+    targets: ".square",
+    translateX: 0,
+    borderRadius: ["0", "50%"],
+    borderColor: "rgb(0, 0, 0)",
+    backgroundColor: "#b2b2b2",
+    easing: "easeInOutQuad",
+  });
+
+  tl.add({
+    targets: ".square",
+    translateY: 0,
+    borderRadius: ["50%", "0"],
+    backgroundColor: "#ffffff",
+    easing: "easeInOutQuad",
+  });
+}
+
 /** Shows preloader on page load and fetches articles. */
 function loadPage() {
+  animatePreloader();
   retrieveArticles(5, trend);
-  setTimeout(showPage, 4000);
+  setTimeout(showPage, 10400);
   trend.color = getColorGradient();
 }
