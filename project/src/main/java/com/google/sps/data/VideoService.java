463a654d63ddc1402f80373899a000934895c385
acnwigwe <acnwigwe@google.com>
2020-07-31 19:24:08 -0400

    Store videos for US trends in Datastore

--- project/src/main/java/com/google/sps/data/VideoService.java
+++ project/src/main/java/com/google/sps/data/VideoService.java
@@ -11,6 +11,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import com.google.sps.data.APISecret;
 import com.google.sps.data.Video;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.*;
+import java.util.Iterator;
 
 public class VideoService {
 
@@ -27,14 +34,13 @@ public class VideoService {
     }
   }
 
-  public List<Video> getRelatedVideos(String topic, String countryCode) throws IOException {
+  public void newRelatedVideos(String topic, String countryCode) throws IOException {
     String url =
         String.format(
             "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=2&order=relevance&regionCode=%s&q=%s&type=video&videoDefinition=high&key=%s",
             countryCode, formulateURLString(topic), API_KEY);
 
     Request request = new Request.Builder().url(url).build();
-    List<Video> videoList = new ArrayList<>();
     try (Response response = httpClient.newCall(request).execute()) {
 
       if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
@@ -47,31 +53,52 @@ public class VideoService {
       for (int i = 0; i < 2; i++) {
         JsonObject videoJson = results.get(i).getAsJsonObject();
         String id = videoJson.get("id").getAsJsonObject().get("videoId").getAsString();
-        Video video = makeVideoObject(id, videoJson.get("snippet").getAsJsonObject());
-        videoList.add(video);
+        storeVideos(id, topic, videoJson.get("snippet").getAsJsonObject());
       }
     }
-    return videoList;
   }
 
-  public Video makeVideoObject(String id, JsonObject video) {
-    String videoLink = String.format("https://www.youtube.com/watch?v=%s", id);
-    return new Video(
-        video.get("title").getAsString(),
-        video.get("description").getAsString(),
-        video
-            .get("thumbnails")
-            .getAsJsonObject()
-            .get("medium")
-            .getAsJsonObject()
-            .get("url")
-            .getAsString(),
-        videoLink);
+  public List<Video> getVideos(String topic) {
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Filter propertyFilter = new FilterPredicate("topic", FilterOperator.EQUAL, topic);
+    Query query = new Query("Video").setFilter(propertyFilter);
+    PreparedQuery results = datastore.prepare(query);
+    List<Video> videos = new ArrayList<>();
+    Iterator<Entity> totalVideos = results.asIterable().iterator();
+    int i = 0;
+    while (totalVideos.hasNext() && i < 3) {
+      Entity entity = totalVideos.next();
+      String title = (String) entity.getProperty("title");
+      String description = (String) entity.getProperty("description");
+      String thumbnail = (String) entity.getProperty("thumbnail");
+      String link = (String) entity.getProperty("link");
+      videos.add(new Video(title, description, thumbnail, link));
+      i++;
+    }
+    return videos;
   }
 
   public String formulateURLString(String topic) {
     Pattern replace = Pattern.compile("\\s+");
     Matcher matcher = replace.matcher(topic);
     return matcher.replaceAll("+");
+  }
+
+  public void storeVideos(String id, String topicName, JsonObject video) {
+    String videoLink = String.format("https://www.youtube.com/watch?v=%s", id);
+    Entity videoEntity = new Entity("Video");
+    videoEntity.setProperty("topic", topicName);
+    videoEntity.setProperty("title", video.get("title").getAsString());
+    videoEntity.setProperty("description", video.get("description").getAsString());
+    videoEntity.setProperty("thumbnail", video
+    .get("thumbnails")
+    .getAsJsonObject()
+    .get("medium")
+    .getAsJsonObject()
+    .get("url")
+    .getAsString());
+    videoEntity.setProperty("link", videoLink);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(videoEntity);
   }
 }
