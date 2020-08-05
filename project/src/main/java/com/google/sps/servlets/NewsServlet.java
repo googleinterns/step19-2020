b8bf55ee8013f06f3236f42860651feb5d0bec11
JBobda <J.BobdaT@gmail.com>
2020-08-05 19:18:37 +0000

    resolved lang/country issue

--- project/src/main/java/com/google/sps/servlets/NewsServlet.java
+++ project/src/main/java/com/google/sps/servlets/NewsServlet.java
@@ -52,7 +52,7 @@ public class NewsServlet extends HttpServlet {
     response.setContentType("text/html;");
     int numArticles = Integer.parseInt(request.getParameter("num"));
     String language = request.getParameter("lang");
-    List<Topic> topics = newsService.populateTopics(trends, "AU", numArticles);
+    List<Topic> topics = newsService.populateTopics(trends, country, numArticles);
     Collections.reverse(topics);
     String jsonString = convertToJson(topics).replaceAll("’", "\u0027");
     jsonString = jsonString.replaceAll("‘", "\u0027");
